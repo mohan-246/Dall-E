37720db2260cce85ad06012495a6bdffde39def8
mohan-246 <mohan5497306@gmail.com>
2023-06-09 16:01:21 +0530

    updated logo animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
-import {logo } from './assets';
+import {logo} from './assets';
 import {Home,CreatePost} from './pages';
+import './App.css';
 
 const App = () => {
   return (
@@ -9,10 +10,13 @@ const App = () => {
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b 
       border-b-[#e6ebf4]'>
       <Link to='/'>
-      <img src={logo} alt='logo' className='w-28 object-contain'/>
+      <div className='logodiv'>
+      <img src={logo} alt='logo' className=' object-contain logo react'/>
+      <p className='logofont px-1 font-extrabold text-xl flex justify-center align-middle'>OpenAI</p>
+      </div>
       </Link>
-      <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
-      Create
+      <Link to="/create-post" className='font-inter hover:bg-[#6450ff] font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+        Create
       </Link>
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({_id,name,prompt,photo}) => {
         src={photo}
         alt={prompt}
       />
-      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10101f] m-2 p-4 rounded-md'>
+      <div className='group-hover:flex infocard flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10101f] m-2 p-4 rounded-md'>
         <p className='text-white text-md overflow-y-auto prompt'> 
           {prompt}</p>
         <div className='mt-5 flex justify-between items-center gap-2'>
